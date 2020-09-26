30857de6071e19425884446cd7ab04f23f8860ad
sawapak-B <71916196+sawapak-B@users.noreply.github.com>
2020-09-26 16:29:29 +0700

    Add files via upload
    Hash Table Exam

---
+++ Exam_003/Exam_003/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Exam_003
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtInDex = new System.Windows.Forms.TextBox();
+            this.txtValues = new System.Windows.Forms.TextBox();
+            this.btAdd = new System.Windows.Forms.Button();
+            this.btDel = new System.Windows.Forms.Button();
+            this.btShow = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btShow);
+            this.groupBox1.Controls.Add(this.btDel);
+            this.groupBox1.Controls.Add(this.btAdd);
+            this.groupBox1.Controls.Add(this.txtValues);
+            this.groupBox1.Controls.Add(this.txtInDex);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 15);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(499, 271);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "HasTable";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(94, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Index :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(94, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 21);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Values :";
+            // 
+            // txtInDex
+            // 
+            this.txtInDex.Location = new System.Drawing.Point(174, 56);
+            this.txtInDex.Name = "txtInDex";
+            this.txtInDex.Size = new System.Drawing.Size(207, 28);
+            this.txtInDex.TabIndex = 1;
+            // 
+            // txtValues
+            // 
+            this.txtValues.Location = new System.Drawing.Point(174, 109);
+            this.txtValues.Name = "txtValues";
+            this.txtValues.Size = new System.Drawing.Size(207, 28);
+            this.txtValues.TabIndex = 1;
+            // 
+            // btAdd
+            // 
+            this.btAdd.Location = new System.Drawing.Point(64, 183);
+            this.btAdd.Name = "btAdd";
+            this.btAdd.Size = new System.Drawing.Size(115, 42);
+            this.btAdd.TabIndex = 2;
+            this.btAdd.Text = "Add";
+            this.btAdd.UseVisualStyleBackColor = true;
+            this.btAdd.Click += new System.EventHandler(this.btAdd_Click);
+            // 
+            // btDel
+            // 
+            this.btDel.Location = new System.Drawing.Point(208, 183);
+            this.btDel.Name = "btDel";
+            this.btDel.Size = new System.Drawing.Size(115, 42);
+            this.btDel.TabIndex = 2;
+            this.btDel.Text = "Delete";
+            this.btDel.UseVisualStyleBackColor = true;
+            this.btDel.Click += new System.EventHandler(this.btDel_Click);
+            // 
+            // btShow
+            // 
+            this.btShow.Location = new System.Drawing.Point(343, 183);
+            this.btShow.Name = "btShow";
+            this.btShow.Size = new System.Drawing.Size(115, 42);
+            this.btShow.TabIndex = 2;
+            this.btShow.Text = "Show All";
+            this.btShow.UseVisualStyleBackColor = true;
+            this.btShow.Click += new System.EventHandler(this.btShow_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(523, 304);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btShow;
+        private System.Windows.Forms.Button btDel;
+        private System.Windows.Forms.Button btAdd;
+        private System.Windows.Forms.TextBox txtValues;
+        private System.Windows.Forms.TextBox txtInDex;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Exam_003/Exam_003/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace Exam_003
+{
+    public partial class Form1 : Form
+    {
+        Hashtable  hashtable;
+
+        public Form1()
+        {
+            InitializeComponent();
+            hashtable = new Hashtable();
+        }
+
+        private void btAdd_Click(object sender, EventArgs e)
+        {
+            if (txtInDex.Text != "" & txtValues.Text != "")
+            {
+                hashtable.Add(txtInDex.Text, txtValues.Text);
+                txtValues.Text = "";
+                txtInDex.Text = "";
+
+            }
+        }
+
+        private void btDel_Click(object sender, EventArgs e)
+        {
+            if (txtInDex.Text != "")
+            {
+                hashtable.Remove(txtInDex.Text);
+                txtValues.Text = "";
+                txtInDex.Text = "";
+            }
+        }
+
+        private void btShow_Click(object sender, EventArgs e)
+        {
+            ICollection iCollect = hashtable.Keys;
+
+            foreach (string index in iCollect)
+            {
+                MessageBox.Show(hashtable[index].ToString());
+            }
+        }
+    }
+}
